9f082e3b6288b37c99ae1dc32bb7333692827f07
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-29 21:42:35 +0900

    로직 재구성

--- src/components/GridBox copy.svelte
+++ src/components/GridBox copy.svelte
@@ -28,9 +28,22 @@
 
     // 가로, 세로, 대각선 각 모양별 개수 (최소 2개씩 설정)
     let [horizontalCount, verticalCount, diagonalCount] = [2, 2, 2];
+
+    let c = 0;
     horizontalCount = Math.floor(givenWords.length/2);
     verticalCount = givenWords.length - horizontalCount;
+    // 대각선 개수는 전체의 40% 미만으로 설정
+    // while ((diagonalCount / givenWords.length < 0.4) && (c<=givenWords.length - 6)) {
+    //     let randNum = extractRandom(0, 2);
+    //     if (randNum === 0) horizontalCount += 1;
+    //     else if (randNum === 1) verticalCount += 1;
+    //     else diagonalCount +=1;
+    //     c++;
+    // }
     console.log(givenWords.length, '개', horizontalCount, verticalCount, diagonalCount);
+
+    // 격자 칸 생성
+    let box = Array.from({ length: rowLen }, () => Array.from({length: colLen}, () => 0));
 
     // 단어 랜덤으로 추출해서 시작 위치 지정하기
     function randomExtractWord (shape) {
@@ -66,69 +79,68 @@
         return [word, startRowIndex, startColIndex];
     };
 
+    function checkArray (shape) {
+        [word, startRowIndex, startColIndex] = randomExtractWord(shape);
+        // 정방향
+        [...word].map((letter, index) => {
+            rowIndex = shape === "horizon" ? startRowIndex : startRowIndex + index;
+            colIndex = shape === "vertical" ? startColIndex : startColIndex + index;
 
-    // function checkArray (shape) {
-    //     [word, startRowIndex, startColIndex] = randomExtractWord(shape);
-    //     // 정방향
-    //     [...word].map((letter, index) => {
-    //         rowIndex = shape === "horizon" ? startRowIndex : startRowIndex + index;
-    //         colIndex = shape === "vertical" ? startColIndex : startColIndex + index;
+            // console.log('중간', box[rowIndex][colIndex]);
+            let boxLetter = box?.[rowIndex]?.[colIndex];
+            if (boxLetter === 0 || boxLetter === letter) check = true;
+            else check = false;
+        })
+        return [check, word, startRowIndex, startColIndex];
+    };
 
-    //         // console.log('중간', box[rowIndex][colIndex]);
-    //         let boxLetter = box?.[rowIndex]?.[colIndex];
-    //         if (boxLetter === 0 || boxLetter === letter) check = true;
-    //         else check = false;
-    //     })
-    //     return [check, word, startRowIndex, startColIndex];
-    // };
+    // 격자에 단어 배열하기
+    function placeWord (shape) {
+        [check, word, startRowIndex, startColIndex] = checkArray(shape);
 
-    // // 격자에 단어 배열하기
-    // function placeWord (shape) {
-    //     [check, word, startRowIndex, startColIndex] = checkArray(shape);
+        while (!check) {
+            checkArray(shape);
+        };
 
-    //     while (!check) {
-    //         checkArray(shape);
-    //     };
+        wordInfo[word] = {
+            'clear': false,
+            'shape': shape, 
+            'points': [],
+            // 'points': [[startRowIndex, startColIndex]],
+        };
 
-    //     wordInfo[word] = {
-    //         'clear': false,
-    //         'shape': shape, 
-    //         'points': [],
-    //         // 'points': [[startRowIndex, startColIndex]],
-    //     };
+        [...word].map((letter, index) => {
+            rowIndex = shape==="horizon" ? startRowIndex : startRowIndex + index;
+            colIndex = shape==="vertical" ? startColIndex : startColIndex + index;
 
-    //     [...word].map((letter, index) => {
-    //         rowIndex = shape==="horizon" ? startRowIndex : startRowIndex + index;
-    //         colIndex = shape==="vertical" ? startColIndex : startColIndex + index;
+            box[rowIndex][colIndex] = letter;
+            wordInfo[word]['points'].push([rowIndex, colIndex]);
+        })
+    };
 
-    //         box[rowIndex][colIndex] = letter;
-    //         wordInfo[word]['points'].push([rowIndex, colIndex]);
-    //     })
-    // };
-
-    // // console.log(wordInfo);
-    // console.log(box);
-    // console.log(wordInfo['ICE']);
+    // console.log(wordInfo);
+    console.log(box);
+    console.log(wordInfo['ICE']);
 
     // 실행
-    // [...Array(horizontalCount)].map((v, i) => placeWord('horizon'));
-    // [...Array(verticalCount)].map((v, i) => placeWord('vertical'));
-    // [...Array(diagonalCount)].map((v, i) => placeWord('diagonal'));
+    [...Array(horizontalCount)].map((v, i) => placeWord('horizon'));
+    [...Array(verticalCount)].map((v, i) => placeWord('vertical'));
+    [...Array(diagonalCount)].map((v, i) => placeWord('diagonal'));
 
-    // console.log('box', box)
-    // const items = box.reduce(function (acc, cur) {
-    //     return acc.concat(cur);
-    // });
-    // console.log('items', items)
-    // console.log(wordInfo)
+    console.log('box', box)
+    const items = box.reduce(function (acc, cur) {
+        return acc.concat(cur);
+    });
+    console.log('items', items)
+    console.log(wordInfo)
 
     // 나머지 칸 랜덤 글자로 채우기
     const alphabet ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    // for (let i in items) {
-    //     if (items[i] === 0) {
-    //         items[i] = alphabet[extractRandom(0, alphabet.length -1)];
-    //     };
-    // };
+    for (let i in items) {
+        if (items[i] === 0) {
+            items[i] = alphabet[extractRandom(0, alphabet.length -1)];
+        };
+    };
 
 
     // 정답 글자
@@ -178,7 +190,7 @@
 <div class='grid-box' style:grid-template-columns={`repeat(${rowLen}, 1fr)`}>
     {#each items as val, index}
         <button class="item-box" data-index={`${index}`} on:dragenter={dragHandler} on:dragend={dragOverHandler} draggable="true">
-            <!-- <div class="item">{val}</div> -->
+            <div class="item">{val}</div>
         </button>
     {/each}
 </div>
